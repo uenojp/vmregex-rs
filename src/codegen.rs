db05f2b268214954cf9957cd6af792de5e0638c0
Takahiro Ueno <uenotakahiro.jp@gmail.com>
2024-04-13 12:05:05 +0900

    Add handling to generate code for quantifier

--- src/codegen.rs
+++ src/codegen.rs
@@ -48,9 +48,9 @@ impl CodeGenerator {
             Ast::Char(c) => self.char(c)?,
             Ast::Concat(concat) => self.concat(concat)?,
             Ast::Or(lhs, rhs) => self.or(*lhs, *rhs)?,
-            Ast::Question(_) => todo!(),
-            Ast::Star(_) => todo!(),
-            Ast::Plus(_) => todo!(),
+            Ast::Question(e) => self.question(*e)?,
+            Ast::Star(e) => self.star(*e)?,
+            Ast::Plus(e) => self.plus(*e)?,
         };
         Ok(())
     }
@@ -64,6 +64,7 @@ impl CodeGenerator {
 
     /// Generate code for Concatenation operator.
     ///
+    /// e1e2
     /// ```txt
     /// e1
     /// e2
@@ -77,6 +78,7 @@ impl CodeGenerator {
 
     /// Generate code for OR operator.
     ///
+    /// e1|e2
     /// ```txt
     ///     split L1, L2
     /// L1: e1 code
@@ -117,11 +119,90 @@ impl CodeGenerator {
         if let Some(Instruction::Jmp(l3)) = self.instractions.get_mut(jmp_pc.0) {
             *l3 = self.pc;
         } else {
+            unreachable!("instruction at PC {} should be Instruction::Jmp", jmp_pc.0);
+        }
+
+        Ok(())
+    }
+
+    /// Generate code for Question operator.
+    ///
+    /// e?
+    /// ```txt
+    ///     split L1, L2
+    /// L1: e code
+    /// L2:
+    /// ```
+    fn question(&mut self, e: Ast) -> Result<(), GenerateCodeError> {
+        assert_eq!(self.instractions.len(), self.pc.0);
+
+        let split_pc = self.pc;
+        let l1 = self.pc.inc()?;
+        self.instractions.push(Instruction::Split(l1, Pc(0))); // L2 TBD.
+        self.expr(e)?;
+        assert_eq!(self.instractions.len(), self.pc.0);
+
+        if let Some(Instruction::Split(_, l2)) = self.instractions.get_mut(split_pc.0) {
+            *l2 = self.pc;
+        } else {
             unreachable!(
-                "instruction at PC {} should be Instruction::Jmp",
+                "instruction at PC {} should be Instruction::Split",
                 split_pc.0
             );
         }
+
+        Ok(())
+    }
+
+    /// Generate code for Start operator.
+    ///
+    /// e*
+    /// ```txt
+    /// L1: split L2, L3
+    /// L2: e code
+    ///     jmp L1
+    /// L3:
+    /// ```
+    fn star(&mut self, e: Ast) -> Result<(), GenerateCodeError> {
+        assert_eq!(self.instractions.len(), self.pc.0);
+
+        let l1 = self.pc;
+        let l2 = self.pc.inc()?;
+        self.instractions.push(Instruction::Split(l2, Pc(0))); // L3 TBD
+        self.expr(e)?;
+        assert_eq!(self.instractions.len(), self.pc.0);
+
+        self.pc.inc()?;
+        self.instractions.push(Instruction::Jmp(l1));
+        assert_eq!(self.instractions.len(), self.pc.0);
+
+        if let Some(Instruction::Split(_, l3)) = self.instractions.get_mut(l1.0) {
+            *l3 = self.pc;
+        } else {
+            unreachable!("instruction at PC {} should be Instruction::Split", l1.0);
+        }
+
+        Ok(())
+    }
+
+    /// Generate code for Plus operator.
+    ///
+    /// e+
+    /// ```txt
+    /// L1: e code
+    ///     split L1, L2
+    /// L2:
+    /// ```
+    fn plus(&mut self, e: Ast) -> Result<(), GenerateCodeError> {
+        assert_eq!(self.instractions.len(), self.pc.0);
+
+        let l1 = self.pc;
+        self.expr(e)?;
+        assert_eq!(self.instractions.len(), self.pc.0);
+
+        let l2 = self.pc.inc()?;
+        self.instractions.push(Instruction::Split(l1, l2));
+        assert_eq!(self.instractions.len(), self.pc.0);
 
         Ok(())
     }
@@ -193,6 +274,55 @@ mod test {
                 /* L5  :10 */ Instruction::Char('g'),
                 /* L6,3:11 */ Instruction::Char('h'),
                 /*     :12 */ Instruction::Match,
+            ]
+        );
+    }
+
+    #[test]
+    fn question() {
+        // a?b
+        let gen = CodeGenerator::default();
+        let ast = Ast::Concat(vec![Ast::Question(Ast::Char('a').into()), Ast::Char('b')]);
+        assert_eq!(
+            gen.generate_code(ast).unwrap(),
+            vec![
+                /*   :0 */ Instruction::Split(Pc(1), Pc(2)),
+                /* L1:1 */ Instruction::Char('a'),
+                /* L2:2 */ Instruction::Char('b'),
+                /*   :3 */ Instruction::Match,
+            ]
+        );
+    }
+
+    #[test]
+    fn star() {
+        // a*b
+        let gen = CodeGenerator::default();
+        let ast = Ast::Concat(vec![Ast::Star(Ast::Char('a').into()), Ast::Char('b')]);
+        assert_eq!(
+            gen.generate_code(ast).unwrap(),
+            vec![
+                /* L1:0 */ Instruction::Split(Pc(1), Pc(3)), // L2, L3
+                /* L2:1 */ Instruction::Char('a'),
+                /*   :2 */ Instruction::Jmp(Pc(0)), // L1
+                /* L3:3 */ Instruction::Char('b'),
+                /*   :4 */ Instruction::Match,
+            ]
+        );
+    }
+
+    #[test]
+    fn plus() {
+        // a+b
+        let gen = CodeGenerator::default();
+        let ast = Ast::Concat(vec![Ast::Plus(Ast::Char('a').into()), Ast::Char('b')]);
+        assert_eq!(
+            gen.generate_code(ast).unwrap(),
+            vec![
+                /* L1:0 */ Instruction::Char('a'),
+                /*   :1 */ Instruction::Split(Pc(0), Pc(2)), // L1, L2
+                /* L2:2 */ Instruction::Char('b'),
+                /*   :3 */ Instruction::Match,
             ]
         );
     }
